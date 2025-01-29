39f87a06e92b6aa5e45382b514b74984c309c73e
zhutoutoutousan <31960995+zhutoutoutousan@users.noreply.github.com>
2025-01-29 12:30:32 +0800

    Add analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { ThemeProvider } from "@/components/theme-provider"
 import Dock from '@/components/Dock'
 import './globals.css'
 import type { Metadata } from 'next'
+import { Analytics } from "@vercel/analytics/react"
 
 export const metadata: Metadata = {
   title: {
@@ -83,6 +84,7 @@ export default function RootLayout({
           >
             {children}
             <Dock />
+            <Analytics />
           </ThemeProvider>
         </ClerkProvider>
       </body>
